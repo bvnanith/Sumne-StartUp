86e17be2b6e3b166ca81f82dd5e4a8ba6deaeef1
Pavan Kumar <pavankumarr.1997@gmail.com>
2023-03-16 13:24:27 +0530

    first edit trial push

--- academic-track/src/App.js
+++ academic-track/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       
+        <h1>Demo text</h1>
       </header>
     </div>
   );
